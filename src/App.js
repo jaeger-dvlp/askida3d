6a98fb32716acd639eb379d9a43fb3e1f8dc8c6a
jaeger-dvlp <omer.kayalar0101@icloud.com>
2021-09-15 17:04:02 +0300

    Import components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import './App.css'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+/* COMPONENTS */
+import Header from './Components/Header'
+
+AOS.init()
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <Header></Header>
+    </div>
+  )
 }
 
 export default App
