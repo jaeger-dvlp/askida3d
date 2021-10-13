73ab4942eea1ac460bafe02aa5fba6c089c753a2
jaeger-dvlp <omer.kayalar0101@icloud.com>
2021-10-13 12:32:13 +0300

    Create components

---
+++ src/Components/PrinterSection.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react'
+
+import PrinterIMG from '../img/sectionPrinterImage.png'
+
+export default class PrinterSection extends Component {
+  render() {
+    return (
+      <div className="container-xl printerSection container-lg p-3 mt-5">
+        <div className="col-12 d-flex  flex-wrap-reverse p-0 m-0 justify-content-center align-items-center">
+          <div
+            data-aos="fade-left"
+            data-aos-delay="400"
+            className="col-12 col-xl-6 col-lg-6 p-3 d-flex flex-wrap justify-content-center align-items-center"
+          >
+            <div className="col-12">
+              <h1 className="p-0 printerSectionHeader">
+                3D Yazıcının Teknik Özellikleri
+              </h1>
+            </div>
+            <div className="col-12 ">
+              <ul className="p-4">
+                <li>%100 yerli üretim ve tam destek.</li>
+
+                <li>Aliquam mauris eu rius.</li>
+
+                <li>Vestibulum daqibus neque mauris eu risus.</li>
+                <li>Nucn dignissim risus metus dignissim risus.</li>
+              </ul>
+            </div>
+          </div>
+          <div
+            data-aos="fade-right"
+            data-aos-delay="400"
+            className="p-0 m-0 col-12 col-xl-6 col-lg-6 p-3 d-flex justify-content-center align-items-center"
+          >
+            <img src={PrinterIMG} alt="x3_img" className=" m-0 col-12" />
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
---
+++ src/Components/PrizeSection.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react'
+
+import PrizeIMG from '../img/prize.png'
+
+export default class PrizeSection extends Component {
+  render() {
+    return (
+      <div className="container-xl printerSection container-lg p-3 mt-5">
+        <div className="col-12 d-flex  flex-wrap p-0 m-0 justify-content-center align-items-center">
+          <div
+            data-aos="fade-left"
+            data-aos-delay="400"
+            className="p-0 m-0 col-12 col-xl-6 col-lg-6 p-3 d-flex justify-content-center align-items-center"
+          >
+            <img src={PrizeIMG} alt="prize_img" className=" m-0 col-12" />
+          </div>
+          <div
+            data-aos="fade-right"
+            data-aos-delay="400"
+            className="col-12 col-xl-6 col-lg-6 p-3 d-flex flex-wrap justify-content-center align-items-center"
+          >
+            <div className="col-12">
+              <h1 className="p-0 printerSectionHeader">
+                Geleceğe Değer Katanlara Minnettarız
+              </h1>
+            </div>
+            <div className="welcomeDesc col-12">
+              Yeni bir teknoloji olan 3D yazıcıları gelecek neslin gelişimi için
+              okullara armağan ederek Öğretime sağlamış olduğunuz bu değer paha
+              biçilemez.
+              <br />
+              Okulumuz ve öğrencilerimiz adına size en içten dileklerimizle
+              teşekkür ederiz. Sizler için küçük bir teşekkür hediyesi
+              hazırladık.
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
