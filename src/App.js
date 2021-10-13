a8d57658bbcb797a21ad5a6059139c68ed0cc19c
jaeger-dvlp <omer.kayalar0101@icloud.com>
2021-10-13 12:31:29 +0300

    Import components

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Header from './Components/Header'
 import WelcomeSection from './Components/WelcomeSection'
 import SelectedMap from './Components/SelectedMap'
 import Map from './Components/Map'
+import PrinterSection from './Components/PrinterSection'
+import PrizeSection from './Components/PrizeSection'
+
 import {MapProvider} from './Components/MapContext'
 AOS.init()
 
@@ -17,6 +20,8 @@ function App() {
       <WelcomeSection></WelcomeSection>
       <Map></Map>
       <SelectedMap></SelectedMap>
+      <PrinterSection />
+      <PrizeSection />
     </MapProvider>
   )
 }
