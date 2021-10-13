8a885b2a56c8ea2d1e727de6cd98590b91046dab
jaeger-dvlp <omer.kayalar0101@icloud.com>
2021-10-13 23:42:29 +0300

    Import all counties

--- src/Components/MapContext.js
+++ src/Components/MapContext.js
@@ -2,7 +2,93 @@ import React, {Component} from 'react'
 let MapContext = React.createContext('')
 
 class MapProvider extends Component {
-  state = {currentMap: 'Ankara', waitingSchoolCount: 0}
+  state = {
+    currentMap: 'Ankara',
+    waitingSchoolCount: 0,
+    counties: [
+      {value: 'Adana', label: 'Adana'},
+      {value: 'Adıyaman', label: 'Adıyaman'},
+      {value: 'Afyonkarahisar', label: 'Afyonkarahisar'},
+      {value: 'Ağrı', label: 'Ağrı'},
+      {value: 'Amasya', label: 'Amasya'},
+      {value: 'Ankara', label: 'Ankara'},
+      {value: 'Antalya', label: 'Antalya'},
+      {value: 'Artvin', label: 'Artvin'},
+      {value: 'Aydın', label: 'Aydın'},
+      {value: 'Balıkesir', label: 'Balıkesir'},
+      {value: 'Bilecik', label: 'Bilecik'},
+      {value: 'Bingöl', label: 'Bingöl'},
+      {value: 'Bitlis', label: 'Bitlis'},
+      {value: 'Bolu', label: 'Bolu'},
+      {value: 'Burdur', label: 'Burdur'},
+      {value: 'Bursa', label: 'Bursa'},
+      {value: 'Çanakkale', label: 'Çanakkale'},
+      {value: 'Çankırı', label: 'Çankırı'},
+      {value: 'Çorum', label: 'Çorum'},
+      {value: 'Denizli', label: 'Denizli'},
+      {value: 'Diyarbakır', label: 'Diyarbakır'},
+      {value: 'Edirne', label: 'Edirne'},
+      {value: 'Elazığ', label: 'Elazığ'},
+      {value: 'Erzincan', label: 'Erzincan'},
+      {value: 'Erzurum', label: 'Erzurum'},
+      {value: 'Eskişehir', label: 'Eskişehir'},
+      {value: 'Gaziantep', label: 'Gaziantep'},
+      {value: 'Giresun', label: 'Giresun'},
+      {value: 'Gümüşhane', label: 'Günüşhane'},
+      {value: 'Hakkâri', label: 'Hakkâri'},
+      {value: 'Hatay', label: 'Hatay'},
+      {value: 'Isparta', label: 'Isparta'},
+      {value: 'Mersin', label: 'Mersin'},
+      {value: 'Istanbul', label: 'Istanbul'},
+      {value: 'İzmir', label: 'İzmir'},
+      {value: 'Kars', label: 'Kars'},
+      {value: 'Kastamonu', label: 'Kastamonu'},
+      {value: 'Kayseri', label: 'Kayseri'},
+      {value: 'Kırklareli', label: 'Kırklareli'},
+      {value: 'Kırşehir', label: 'Kırşehir'},
+      {value: 'Kocaeli', label: 'Kocaeli'},
+      {value: 'Konya', label: 'Konya'},
+      {value: 'Kütahya', label: 'Kütahya'},
+      {value: 'Malatya', label: 'Malatya'},
+      {value: 'Manisa', label: 'Manisa'},
+      {value: 'Kahramanmaraş', label: 'Kahramanmaraş'},
+      {value: 'Mardin', label: 'Mardin'},
+      {value: 'Muğla', label: 'Muğla'},
+      {value: 'Muş', label: 'Muş'},
+      {value: 'Nevşehir', label: 'Nevşehir'},
+      {value: 'Niğde', label: 'Niğde'},
+      {value: 'Ordu', label: 'Ordu'},
+      {value: 'Rize', label: 'Rize'},
+      {value: 'Sakarya', label: 'Sakarya'},
+      {value: 'Samsun', label: 'Samsun'},
+      {value: 'Siirt', label: 'Siirt'},
+      {value: 'Sinop', label: 'Sinop'},
+      {value: 'Sivas', label: 'Sivas'},
+      {value: 'Tekirdap', label: 'Tekirdağ'},
+      {value: 'Tokat', label: 'Tokat'},
+      {value: 'Trabzon', label: 'Trabzon'},
+      {value: 'Tunceli', label: 'Tunceli'},
+      {value: 'Şanlıurfa', label: 'Şanlıurfa'},
+      {value: 'Uşak', label: 'Uşak'},
+      {value: 'Van', label: 'Van'},
+      {value: 'Yozgat', label: 'Yozgat'},
+      {value: 'Zonguldak', label: 'Zonguldak'},
+      {value: 'Aksaray', label: 'Aksaray'},
+      {value: 'Bayburt', label: 'Bayburt'},
+      {value: 'Karaman', label: 'Karaman'},
+      {value: 'Kırıkkale', label: 'Kırıkkale'},
+      {value: 'Batman', label: 'Batman'},
+      {value: 'Şırnak', label: 'Şırnak'},
+      {value: 'Bartın', label: 'Bartın'},
+      {value: 'Ardahan', label: 'Ardahan'},
+      {value: 'Iğdır', label: 'Iğdır'},
+      {value: 'Yalova', label: 'Yalova'},
+      {value: 'Karabük', label: 'Karabük'},
+      {value: 'Kilis', label: 'Kilis'},
+      {value: 'Osmaniye', label: 'Osmaniye'},
+      {value: 'Düzce', label: 'Düzce'}
+    ]
+  }
 
   setMap = (currentMap) => {
     this.setState({
@@ -12,11 +98,13 @@ class MapProvider extends Component {
   }
   render() {
     const {children} = this.props
-    const {currentMap, waitingSchoolCount} = this.state
+    const {currentMap, waitingSchoolCount, counties} = this.state
     const {setMap} = this
 
     return (
-      <MapContext.Provider value={{currentMap, setMap, waitingSchoolCount}}>
+      <MapContext.Provider
+        value={{currentMap, setMap, waitingSchoolCount, counties}}
+      >
         {children}
       </MapContext.Provider>
     )
