e7f628c217a4031be7ec6b513713c403c4241834
jaeger-dvlp <omer.kayalar0101@icloud.com>
2021-10-13 10:35:04 +0300

    Change text color etc.

--- src/Components/SelectedMap.js
+++ src/Components/SelectedMap.js
@@ -6,19 +6,37 @@ export class SelectedMap extends Component {
 
   constructor(props) {
     super(props)
-    this.state = {theMap: ''}
+
+    this.writeAllSchools()
   }
 
   componentDidMount() {}
 
+  writeAllSchools() {}
+
   render() {
-    const {currentMap} = this.context
+    const {/*waitingSchoolCount, */ currentMap} = this.context
+    //{waitingSchoolCount}
     return (
-      <div className="container p-3">
+      <div
+        data-aos="fade-in"
+        data-aos-delay="300"
+        className="container-lg container-xl pt-5 p-xl-3 p-lg-3 p-1"
+      >
         <div className="col-12 selectedMap-cont p-0 m-0 d-flex row">
-          <div className="col-12 selected-map">
-            {currentMap} Bekleme Listesi
+          <div className="col-12 selected-map pb-1">
+            <h1 className>
+              <span>{currentMap} bekleme listesi</span>
+            </h1>
           </div>
+          <div className="col-12 text-left selected-map-desc">
+            Okullar MEB sistemi üzerinden güncel olarak listelenmektedir ve 3D
+            yazıcı bağışlanabilecek okullar aşağıdadır.
+          </div>
+          <div className="col-12 pt-2 text-left selected-map-desc">
+            Bekleyen Okul Sayısı : 0
+          </div>
+          <div className="col-12 selected-map-list text-left m-0 p-4 mt-4 row"></div>
         </div>
       </div>
     )
